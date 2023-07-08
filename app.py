32d59cb6f99acec0e263dca69b56b1b6bd2a611d
Shinya Kinjo <shinya.work0720@gmail.com>
2023-07-09 10:57:38 +1200

    [update]app.py to root dir

---
+++ app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from mangum import Mangum
+from fastapi.responses import JSONResponse
+import uvicorn
+
+app = FastAPI()
+handler = Mangum(app)
+
+@app.get("/")
+def health_check():
+    return JSONResponse({"status": "OK"})
+
+
+@app.get("/{text}")
+def read_item(text: str):
+    return JSONResponse({"result": text})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)

10617777569006df04d9aa74c67d665910abdd18
Shinya Kinjo <shinya.work0720@gmail.com>
2023-07-09 12:34:30 +1200

    [test]add user after confirming running

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from fastapi.responses import JSONResponse
-# from src.usecase.user.get import get as uget
+from src.usecase.user.get import get as uget
 
 app = FastAPI()
 handler = Mangum(app)
@@ -10,7 +10,7 @@ handler = Mangum(app)
 def health_check():
     return JSONResponse({"status": "OK"})
 
-# @app.get("/user/{user_id}")
-# def get_user(user_id: int):
-#     res = uget(user_id)
-#     return JSONResponse({"res": res})
+@app.get("/user/{user_id}")
+def get_user(user_id: int):
+    res = uget(user_id)
+    return JSONResponse({"res": res})

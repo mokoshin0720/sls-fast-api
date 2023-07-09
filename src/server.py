36049bac0ee1c71afc491f9cb0ffd1f7a81b82ef
Shinya Kinjo <shinya.work0720@gmail.com>
2023-07-10 08:38:03 +1200

    [add]init.py

--- src/server.py
+++ src/server.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from fastapi.responses import JSONResponse
-# from src.usecase.user.get import get as uget
 
 app = FastAPI()
 handler = Mangum(app)
@@ -9,8 +8,3 @@ handler = Mangum(app)
 @app.get("/")
 def health_check():
     return JSONResponse({"status": "OK"})
-
-# @app.get("/user/{user_id}")
-# def get_user(user_id: int):
-#     res = uget(user_id)
-#     return JSONResponse({"res": res})
--- src/usecase/user/get.py
+++
@@ -1,3 +0,0 @@
-def get(user_id: int):
-    print("user_id={}".format(user_id))
-    return "user_id={} from usecase".format(user_id)

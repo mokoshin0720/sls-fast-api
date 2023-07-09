03275fd8553500cd30e8e0e7151df7cd1a3c47f6
Shinya Kinjo <shinya.work0720@gmail.com>
2023-07-10 08:45:55 +1200

    [update]using usecase layer after running lambda

--- src/server.py
+++ src/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from fastapi.responses import JSONResponse
+import src.usecase.user as user
 
 app = FastAPI()
 handler = Mangum(app)
@@ -8,3 +9,7 @@ handler = Mangum(app)
 @app.get("/")
 def health_check():
     return JSONResponse({"status": "OK"})
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return JSONResponse({"user_id": user.say_hello(user_id)})
